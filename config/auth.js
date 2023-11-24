9113872f86d7d332ccff96cc206c68ab76e4856b
BolverBlitz <bolverblitz@ebg.pw>
2023-11-24 20:20:17 +0100

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -3,15 +3,15 @@ module.exports = {
         "url": "https://github.com/login/oauth/authorize",
         "url_auth": "https://github.com/login/oauth/access_token",
         "url_scope": ["user:read", "user:email"],
-        "clientID": "",
-        "clientSecret": "",
+        "clientID": process.env.GITHUB_CLIENT_ID,
+        "clientSecret": process.env.GITHUB_SECRET
     },
     "Google": {
         "url": "https://accounts.google.com/o/oauth2/v2/auth",
         "url_auth": "https://oauth2.googleapis.com/token",
         "url_redirect": "http://localhost/auth/google/callback",
         "url_scope": ["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"],
-        "clientID": "",
-        "clientSecret": "",
+        "clientID": process.env.GOOGLE_CLIENT_ID,
+        "clientSecret": process.env.GOOGLE_SECRET
     }
 }
