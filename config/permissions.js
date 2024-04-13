b236fd2355000ea7d3725a3d6c769b665cd533b0
BolverBlitz <bolverblitz@ebg.pw>
2024-04-13 04:49:16 +0200

    Added settings button and some permissions checking

--- config/permissions.js
+++ config/permissions.js
@@ -2,6 +2,9 @@
     app_permissions: List all permissions that are native to the application itself.
     All those permissions do not have a read or write part.
     However all those permissions need read & write to be true in order for a user to get them granted!
+
+    There are 2 groups in the panel, reg and member. For example for the events and activities.
+    This needs to be represented by a group.member permission. This will grand the member check for the group.
 */
 
 module.exports = {
@@ -19,14 +22,25 @@ module.exports = {
             "inherit": ["admin"]
         },
         "user": {
-            "permissions": [],
+            "permissions": [
+            ],
             "inherit": [
                 "app"
+            ]
+        },
+        "member": {
+            "permissions": [
+                "group.membership"
+            ],
+            "inherit": [
+                "user"
             ]
         },
         "admin": {
             "permissions": [
                 "*"
+            ],
+            "inherit": [
             ]
         }
     }
