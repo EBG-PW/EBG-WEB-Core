81d610dcec44fd540ade291616cfad0a2e7d7bb1
BolverBlitz <bolverblitz@ebg.pw>
2024-05-08 14:21:40 +0900

    Fixed plublicStaticCache because the objOptions parameter wasn´t truly optional

--- middleware/cacheRequest.js
+++ middleware/cacheRequest.js
@@ -16,25 +16,29 @@ const plublicStaticCache = (duration, objOptions = []) => {
             const oldSend = res.send;
             const oldJson = res.json;
 
-            if (objOptions.length === 0) throw new CustomError(`No object options provided for ${req.route.pattern}`).withStatus(500).withInfo("Can´t generate cache key");
+            let cachHash; // The hash or the cache key
 
-            let importantDataObject = {};
-            for (let i = 0; objOptions.length > i; i++) {
-                // Check if key exists, the array can have layered strings like "user.id" referring to req.user.id
-                const keySplit = objOptions[i].split(".");
+            if (objOptions.length !== 0) {
+                let importantDataObject = {};
+                for (let i = 0; objOptions.length > i; i++) {
+                    // Check if key exists, the array can have layered strings like "user.id" referring to req.user.id
+                    const keySplit = objOptions[i].split(".");
 
-                const value = keySplit.reduce((prev, curr) => {
-                    return prev ? prev[curr] : null;
-                }, req);
-                if (value !== null) {
-                    // Use the original key (objOptions[i]) as the key in importantDataObject
-                    importantDataObject[objOptions[i]] = value;
-                } else {
-                    throw new CustomError(`Could not construct cache key for this request`).withStatus(500)
+                    const value = keySplit.reduce((prev, curr) => {
+                        return prev ? prev[curr] : null;
+                    }, req);
+                    if (value !== null) {
+                        // Use the original key (objOptions[i]) as the key in importantDataObject
+                        importantDataObject[objOptions[i]] = value;
+                    } else {
+                        throw new CustomError(`Could not construct cache key for this request`).withStatus(500)
+                    }
                 }
-            }
 
-            const cachHash = oHash(importantDataObject, { algorithm: process.env.INTERNAL_HASH || "sha3-256" })
+                cachHash = oHash(importantDataObject, { algorithm: process.env.INTERNAL_HASH || "sha3-256" })
+            } else {
+                cachHash = "default"
+            }
 
             res.send = function (data) {
                 res.body = data;
