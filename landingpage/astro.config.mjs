ab86cd9ef6cbd5352a04125b8c809a4a17471e00
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2024-05-28 22:17:05 +0200

    added team

--- landingpage/astro.config.mjs
+++ landingpage/astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from "astro/config";
+
 import icon from "astro-icon";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [icon()]
+   integrations: [icon(),
+   compress({
+      html: {
+         removeComments: true
+      }
+   })
+   ],
+   compressHTML: true
 });
