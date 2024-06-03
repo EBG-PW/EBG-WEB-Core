c06ec5e4158e93b66ce087ba4e4d17082206226b
BolverBlitz <bolverblitz@ebg.pw>
2024-06-03 04:42:00 +0200

    Added more oAuth logic

--- static_api/oauth.js
+++ static_api/oauth.js
@@ -22,7 +22,7 @@ const oAuthSubmitShema = Joi.object({
     code: Joi.string().alphanum().required(),
 });
 
-//console.log(oAuthPermissions.genPermission(["USER:ID:READ", "USER:USERNAME:READ", "USER:EMAIL:READ", "USER:REALNAME:READ", "USER:BIO:READ", "USER:AVATAR:READ", "USER:GROUP:READ", "USER:ADRESS:READ", "SETTINGS:DESIGN:READ", "SETTINGS:LANG:READ", "EVENTS:LIST:READ", "PROJECTS:LIST:READ"]))
+//console.log(oAuthPermissions.genPermission(["USER:ID:READ", "USER:USERNAME:READ", "USER:EMAIL:READ", "USER:REALNAME:READ", "USER:BIO:READ", "USER:AVATAR:READ", "USER:GROUP:READ", "USER:ADRESS:READ", "SETTINGS:DESIGN:READ", "SETTINGS:LANG:READ", "EVENTS:LIST:READ", "PROJECTS:LIST:READ", "INTEGRATION:TELEGRAM:READ"]))
 
 router.get('/', verifyRequest('app.web.login'), limiter(10), async (req, res) => {
     const { client_id, scope } = await OAuthRequestShema.validateAsync(req.query);
@@ -35,6 +35,42 @@ router.get('/', verifyRequest('app.web.login'), limiter(10), async (req, res) =>
         avatar_url: oAuthClientResponse.avatar_url,
         permissions: oAuthPermissions.listPermissionsTranslation(scope)
     });
+});
+
+router.post('/', verifyRequest('app.web.login'), limiter(10), async (req, res) => {
+    const { client_id, scope } = await OAuthRequestShema.validateAsync(await req.json());
+    const user_id = req.user.user_id;
+
+    const oAuthClientResponse = await oAuth.get_client(client_id, scope, user_id)
+    console.log(oAuthClientResponse)
+
+    const code = randomstring.generate(128);
+
+    await oAuthSession.addSession(code, {"user_id": user_id, "oAuthApp_id": oAuthClientResponse.id});
+
+    res.json({
+        redirect_uri: oAuthClientResponse.redirect_url,
+        code: code
+    });
+});
+
+router.post('/authorize', limiter(10), async (req, res) => {
+    const { code } = await oAuthSubmitShema.validateAsync(await req.json());
+
+    const session_data = await oAuthSession.getSession(code);
+
+    console.log(session_data) // { user_id: 1, oAuthApp_id: '1' }
+
+    if (!session_data) {
+        throw new OAuthError("Invalid code").withInfo("The code is invalid or has expired");
+    }
+
+    const access_token = randomstring.generate(64);
+    const refresh_token = randomstring.generate(128);
+
+    // Insert into DB, add timestamps to the tokens to invalidate access_token after x time
+
+    res.json({ access_token, refresh_token });
 });
 
 module.exports = router;
