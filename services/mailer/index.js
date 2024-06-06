06ec5f534b0079c24a405854ad417e38fa29b647
Marc <bolverblitz@ebg.pw>
2024-06-06 02:20:21 +0200

    Ignore SSL for SMTP (Temporary)

--- services/mailer/index.js
+++ services/mailer/index.js
@@ -46,11 +46,14 @@ const connection = {
 const emailtransporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 (async () => {
