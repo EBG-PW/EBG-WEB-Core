2c3c5994904dd89268b89be0d1b3d9024bf4db29
DarkIntaqt <61588850+DarkIntaqt@users.noreply.github.com>
2024-08-11 22:49:07 +0200

    add solar

--- landingpage/astro.config.mjs
+++ landingpage/astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
 import compress from "astro-compress";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-   integrations: [icon(),
-   compress({
-      html: {
-         removeComments: true
-      }
-   })
-   ],
-   compressHTML: true
+  integrations: [icon(), compress({
+    html: {
+      removeComments: true
+    }
+  }), preact({ compat: true })],
+  compressHTML: true,
 });
