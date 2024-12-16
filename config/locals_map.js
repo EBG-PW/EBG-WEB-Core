eeabe50db38976d4052b8d8322e8f62018d1310b
BolverBlitz <bolverblitz@ebg.pw>
2024-12-16 01:34:18 +0100

    Fixed missing translation string on settings-account

--- config/locals_map.js
+++ config/locals_map.js
@@ -7,7 +7,7 @@ module.exports = {
     "/requestresetpassword": ["ResetPassword"],
     "/auth/oauth": ["OAuth"],
     "/auth/reset-password": ["ResetPassword"],
-    "/profile-dropdown/settings-account": ["Dashboard", "User_Groups", "Settings"],
+    "/profile-dropdown/settings-account": ["Dashboard", "User_Groups", "Settings", "Login"],
     "/profile-dropdown/settings-integrations": ["Dashboard", "User_Groups", "Settings"],
     "/profile-dropdown/settings-links": ["Dashboard", "User_Groups", "Settings"],
     "/profile-dropdown/settings-misc": ["Dashboard", "User_Groups", "Settings"],
